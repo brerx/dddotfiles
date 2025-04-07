bb378e3c944047046b3c829954514996e88db6eb
Thomas Müller <thomas.mueller@peopleware.com>
2025-04-07 13:05:52 +0200

    Add overrides

---
+++ vim/nvim-lazy-plugins.override.lua
@@ -0,0 +1,8 @@
+return {
+  { 'gcmt/wildfire.vim' },
+  { 'tpope/vim-commentary' },
+  { 'tpope/vim-speeddating' },
+  { 'winston0410/cmd-parser.nvim' },
+  { 'kyoh86/vim-jsonl' },
+  { 'jiangmiao/auto-pairs', enabled = false }
+}
